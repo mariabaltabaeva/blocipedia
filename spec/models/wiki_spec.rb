fc0980f6c421ff8cd76302c2b4385043097c97b2
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-05-11 12:27:40 -0400

    In process

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-
+  let(:user) { User.create!(email:'test@gmail.com', password: 'password1', confirmed_at: Time.now) }
   let(:wiki) { Wiki.create!(title: "New Wiki title", body: "New Wiki Body", private: true, user: user) }
 
   describe "attributes" do
      it "has title, body, private, user  attributes" do
-       expect(wiki).to have_attributes(title: "New Wiki title", body: "New Wiki Body", private: true, user)
+       expect(wiki).to have_attributes(title: "New Wiki title", body: "New Wiki Body", private: true, user: user)
      end
    end
  end
