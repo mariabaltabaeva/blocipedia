0e54d5b2b0a3c250149d9e9b3f973686a2edc6ee
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-05-15 18:22:27 -0400

    With corrections

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -12,9 +12,7 @@ class WikisController < ApplicationController
   end
 
   def create
-     @wiki = Wiki.new
-     @wiki.title = params[:wiki][:title]
-     @wiki.body = params[:wiki][:body]
+     @wiki = Wiki.new(wiki_params)
      @wiki.user = current_user
 
      if @wiki.save
@@ -32,8 +30,7 @@ class WikisController < ApplicationController
 
   def update
     @wiki = Wiki.find(params[:id])
-    @wiki.title = params[:wiki][:title]
-    @wiki.body = params[:wiki][:body]
+    @wiki.assign_attributes(wiki_params)
 
      if @wiki.save
        flash[:notice] = "Wiki was updated."
@@ -55,4 +52,9 @@ class WikisController < ApplicationController
        render :show
      end
    end
+
+   private
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
 end
