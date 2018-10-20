1a04e10aff1cd5ea90a2bc72d40744138e083b3e
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-10-20 16:20:24 -0400

    With latest updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :about]
   protect_from_forgery with: :exception
   include Pundit
   protect_from_forgery
